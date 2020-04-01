8e21d3e12056b57b25ef0119dd3b69d454bdc32e
Bastien Robert <bastienrobert@outlook.com>
2020-04-01 10:59:48 +0200

    chore(stylelint): add config for mobile and components

--- packages/server/rollup.config.js
+++ packages/server/rollup.config.js
@@ -1,11 +1,11 @@
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import ts from '@wessberg/rollup-plugin-ts'
+import { eslint } from 'rollup-plugin-eslint'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import graphql from 'rollup-plugin-graphql'
 import json from '@rollup/plugin-json'
-import { eslint } from 'rollup-plugin-eslint'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 
 const env = JSON.stringify(
